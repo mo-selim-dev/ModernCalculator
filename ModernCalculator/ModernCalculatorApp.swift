4860ef3da38cd89dd7a17feda4bae9ed4a247130
Mohammed Selim <mo.selim.dev@gmail.com>
2024-11-19 00:28:01 +0200

    Initial Commit

---
+++ ModernCalculator/ModernCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernCalculatorApp.swift
+//  ModernCalculator
+//
+//  Created by Mohamed Selim on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ModernCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
